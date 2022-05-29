391e51720e242eb1a64666259db0aab937cbb300
Fred <freddcfonseca@gmail.com>
2022-05-29 15:53:10 -0300

    :construction: uploading demo

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:'https://wda-library-db.herokuapp.com//api'
+  baseURL:'https://wda-library-db.herokuapp.com/api'
 })
 
 export default api
